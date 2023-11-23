af42cefb91562a96e5f01f38ca463aa37a5eeae6
Alsaad <mrrgx.22@gmail.com>
2023-11-23 13:37:42 +0300

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by Mohammed on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
